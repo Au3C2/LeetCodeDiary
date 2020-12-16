fde32b7b37a098e515db2b38302c799237083b29
Au3C2 <au3c2@hotmail.com>
2020-12-16 09:06:22 +0800

    20201216更新

--- Code/test.py
+++ Code/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2020-12-10 09:16:26
+LastEditTime: 2020-12-16 09:03:27
 '''
 import collections    
 import heapq
@@ -11,27 +11,24 @@ from collections import deque
 import functools
 # from numba import njit
 import numpy as np
+import bisect
 
-def characterReplacement(bills) -> bool:
-    change5, change10 = 0,0
-    for num in bills:
-        if num ==5:
-            change5 += 1
-        if num == 10:
-            if change5 > 0:
-                change5 -= 1
-                change10 += 1
-            else:
-                return False
-        if num == 20:
-            if (change5>0 and change10>0):
-                change10 -= 1
-                change5 -= 1
-            elif change5 >= 3:
-                change5 -= 3
-            else:
-                return False
+def function(pattern: str, s: str):
+    p_dict = {}
+    w_dict = {}
+    w_list = s.split(' ')
+    if len(pattern) != len(w_list):
+        return False
+    for c,word in zip(pattern,s.split(' ')):
+        if c not in p_dict:
+            p_dict[c] = word
+        elif p_dict[c] != word:
+            return False
+        
+        if word not in w_dict:
+            w_dict[word] = c
+        elif w_dict[word] != c: 
+            return False
     return True
-    
-something = characterReplacement([5,5,10,10,20])
+something = function("aaa","aa aa aa aa")
 print(something)
