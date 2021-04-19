24f552106ca65a9f36a71f5716333fe7375fe46f
Au3C2 <au3c2@hotmail.com>
2021-04-19 12:34:25 +0800

    更新

--- utils/test.py
+++ utils/test.py
@@ -3,42 +3,35 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-04-10 10:58:26
+LastEditTime: 2021-04-19 10:17:48
 '''
 import collections    
 import heapq
 from collections import deque
-import functools
+from functools import *
 import numpy as np
 import bisect
 import math
 from scipy.special import comb, perm
 from utils import *
+from sortedcontainers import SortedList
 class Solution:
     def __init__(self):
         pass
-        
-    def function(self,n):
-        n = abs(n)
-        while n > 0:
-            if n == 1:
-                return True
-            elif n % 2 == 0:
-                n = n//2
-            elif n % 3 == 0:
-                n = n//3
-            elif n % 5 == 0:
-                n = n//5
-            else:
-                return False
-        return True        
+    def function(self,nums,val):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
 
 null = None
 # root = buildTree([-10,9,20,null,null,15,7])
 # t2 = buildTree([2,1,3,null,4,null,7])      
 # head = buildList([1,2,3,4,5])      
 S = Solution()
-# something = S.function(2)
+something = S.function(nums = [0,1,2,2,3,0,4,2], val = 2)
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
-something = S.function(-2147483648)
+# something = S.function([" /","/ "])
 print(something)
