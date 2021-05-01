9f0f1c8f0e3a67d519a7a2f7a4b465c1a3581528
Au3C2 <au3c2@hotmail.com>
2021-05-01 15:37:08 +0800

    202104第5周更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-04-28 12:19:07
+LastEditTime: 2021-04-30 10:08:22
 '''
 import collections    
 import heapq
@@ -18,23 +18,18 @@ from sortedcontainers import SortedList
 class Solution:
     def __init__(self):
         pass
-    def function(self, c):
-        b = 0
-        t = c - b**2
-        while t >= 0 :
-            a = math.sqrt(t)
-            if a % 1 == 0:
-                return True
-            else:
-                b += 1
-                t = c - b**2
-        return False
+    def function(self, nums):
+        counter = collections.Counter(nums)
+        for key,value in counter.items():
+            if value == 1: 
+                return key
+
 null = None
 # root = buildTree([-10,9,20,null,null,15,7])
 # t2 = buildTree([2,1,3,null,4,null,7])      
 # head = buildList([1,2,3,4,5])      
 S = Solution()
-something = S.function(c = 0)
+something = S.function(nums = [0,1,0,1,0,1,99])
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # something = S.function([" /","/ "])
 print(something)
