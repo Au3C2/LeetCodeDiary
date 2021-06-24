8f2165ed6f34014c078fccb9eea35acc7bf251fb
Au3C2 <au3c2@hotmail.com>
2021-06-24 12:13:00 +0800

    0624更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-06-22 11:06:36
+LastEditTime: 2021-06-24 12:10:37
 '''
 import collections
 import heapq
@@ -23,20 +23,15 @@ class Solution:
     def __init__(self):
         pass
 
-    def function(self, s: str) -> List[str]:
-        def recursion(s:str):
-            if not s: 
-                return ''
-            else:
-                ans = []
-                for i,c in enumerate(s):
-                    res = recursion(s[:i]+s[i+1:])
-                    if res == '':
-                        ans.append(c)
-                    for r in res:
-                        ans.append(c+r)
-            return ans
-        ans = recursion(s)
+    def function(self, points: List[List[int]]) -> int:
+        ans = 1
+        n = len(points)
+        for i in range(n-1):
+            counter = Counter()
+            for j in range(i+1,n):
+                dx, dy = points[i][0] - points[j][0], points[i][1] - points[j][1]
+                counter[dy/dx if dx else 'inf'] += 1
+            ans = max(ans,max(counter.values())+1)
         return ans
         pass
         
@@ -44,7 +39,7 @@ class Solution:
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(s = "abc")
+something = S.function(points = [[1,1],[2,2],[3,3]])
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # something = S.function([" /","/ "])
 print(something)
