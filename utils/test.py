0ae37b9f5ac5932b80cfc5b61984d76cf6277c3c
Au3C2 <au3c2@hotmail.com>
2021-06-25 13:34:49 +0800

    0625更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-06-24 12:10:37
+LastEditTime: 2021-06-25 13:13:47
 '''
 import collections
 import heapq
@@ -23,23 +23,42 @@ class Solution:
     def __init__(self):
         pass
 
-    def function(self, points: List[List[int]]) -> int:
-        ans = 1
-        n = len(points)
-        for i in range(n-1):
-            counter = Counter()
-            for j in range(i+1,n):
-                dx, dy = points[i][0] - points[j][0], points[i][1] - points[j][1]
-                counter[dy/dx if dx else 'inf'] += 1
-            ans = max(ans,max(counter.values())+1)
-        return ans
+    def function(self, deadends: List[str], target: str) -> int:
+        # deadends转化为set加速in check
+        if "0000" in (deadends := set(deadends)):
+            return -1
+        elif target == "0000":
+            return 0
+
+        # BFS由一个 双端队列q 和 一个字典d {存遍历过的字符串: 需要旋转次数}组成，组成namedtuple
+        BFS = namedtuple("BFS", "q, d")
+        s, e = BFS(deque(["0000"]), {"0000": 0}), BFS(deque([target]), {target: 0})
+        while s.q and e.q:
+            # 选一个短的q拓展, 降低解空间复杂度
+            if len(s.q) > len(e.q):
+                s, e = e, s
+            for _ in range(len(s.q)):
+                c = s.q.popleft()
+                for nxt in [
+                    # 需要操作的位先转化为int, 加减1后%10，在转化为str，再和剩余位拼接
+                    c[:i] + str((int(v) + x) % 10) + c[i + 1 :]
+                    for x in (-1, 1)
+                    for i, v in enumerate(c)
+                ]:
+                    # 双向BFS交集，求得解空间
+                    if nxt in e.d:
+                        return s.d[c] + e.d[nxt] + 1
+                    elif nxt not in s.d and nxt not in deadends:
+                        s.q.append(nxt)
+                        s.d[nxt] = s.d[c] + 1
+        return -1
         pass
         
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(points = [[1,1],[2,2],[3,3]])
+something = S.function( deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888")
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # something = S.function([" /","/ "])
 print(something)
