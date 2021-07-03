8e0f57e6b436779fa0c296c33616aa28bcd21c8b
Au3C2 <au3c2@hotmail.com>
2021-07-03 13:03:31 +0800

    202107第1周更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-07-02 09:49:51
+LastEditTime: 2021-07-03 10:54:55
 '''
 import collections
 import heapq
@@ -24,19 +24,20 @@ class Solution:
     def __init__(self):
         pass
 
-    def function(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        i = 0
-        while coins >= 0 and i <len(costs):
-            coins -= costs[i]
-            i += 1
-        return i if coins >=0 else i - 1
+    def function(self, s: str) -> str:
+        counter = Counter(s)
+        res = ''
+        counter = list(counter.items())
+        counter.sort(key=lambda x:x[1],reverse=True)
+        for key, value in counter:
+            res += key*value
+        return res
         pass
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(costs = [1,6,3,1,2,5], coins = 20)
+something = S.function("Aabb")
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # something = S.function([" /","/ "])
 print(something)
