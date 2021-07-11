17f71b1f5f11794224998e8d9d5e5a7bc1c43a55
Au3C2 <au3c2@hotmail.com>
2021-07-11 13:33:38 +0800

    0711update

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-07-10 11:20:53
+LastEditTime: 2021-07-11 13:30:30
 '''
 import collections
 import heapq
@@ -25,21 +25,14 @@ class Solution:
     def __init__(self):
         pass
 
-    def function(self,  nums: List[int]) -> int:
-        candidate = -1
-        count = 0
-        for num in nums:
-            if count == 0:
-                candidate = num
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
-        count = 0
-        for num in nums:
-            if num == candidate:
-                count += 1
-        return candidate if count > len(nums)/2 else -1
+    def function(self,  citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        for i,c in enumerate(citations):
+            h = n - i
+            if h <= c:
+                return h
+        return 0
         
         pass
         
@@ -47,7 +40,7 @@ class Solution:
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function([1,2,3])
+something = S.function(citations = [3,0,6,1,5])
 # something = S.function(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
 # something = S.function([" /","/ "])
 print(something)
