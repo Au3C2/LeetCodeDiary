cd6f46038e0290261bc03dae30e96f49c99c06df
Au3C2 <au3c2@hotmail.com>
2021-07-19 14:53:51 +0800

    202107第3周更新

--- utils/test.py
+++ utils/test.py
@@ -3,14 +3,13 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-07-16 10:21:43
+LastEditTime: 2021-07-19 14:17:40
 '''
-import collections
 import copy
 import heapq
 import math
 from bisect import *
-from collections import defaultdict, deque
+from collections import *
 from functools import *
 from itertools import *
 from typing import *
@@ -24,36 +23,22 @@ from utils import *
 null = None
 # from sortedcontainers import SortedList
 
-
 class Solution:
     def __init__(self):
         self.MAX_SIZE = 10 ** 9 + 7
         pass
                     
-    def function(self, nums: List[int], target: int) -> int:
-        idx = bisect.bisect_left(nums, target)
-        lp, rp = idx-1, idx
-        ans = 0
-        while lp >= 0:
-            if nums[lp] == target:
-                ans += 1
-            else:
-                break
-            lp -= 1
-        lp += 1
-        while rp < len(nums):
-            if nums[rp] == target:
-                ans += 1
-            else:
-                break
-            rp += 1
-        rp -= 1
-        return [lp, rp] if ans else [-1,-1]
+    def function(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            d[''.join(sorted(s))].append(s)
+        return list(d.values())
+            
         
-        
+ 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(nums = [1], target = 1)
+something = S.function(["eat", "tea", "tan", "ate", "nat", "bat"])
 print(something)
