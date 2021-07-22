c15e3abea1456713502fdd167d2810661a549df1
Au3C2 <au3c2@hotmail.com>
2021-07-22 16:18:30 +0800

    0722更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2020-11-24 12:46:57
 LastEditors: Au3C2
-LastEditTime: 2021-07-19 14:17:40
+LastEditTime: 2021-07-22 16:11:19
 '''
 import copy
 import heapq
@@ -28,17 +28,29 @@ class Solution:
         self.MAX_SIZE = 10 ** 9 + 7
         pass
                     
-    def function(self, strs: List[str]) -> List[List[str]]:
-        d = defaultdict(list)
-        for s in strs:
-            d[''.join(sorted(s))].append(s)
-        return list(d.values())
-            
+    def function(self, head: 'Node') -> 'Node':
+        h = Node(-1,head)
+        p = h.next
+        while p:
+            node = Node(p.val, p.next)
+            p.next = node
+            p = node.next
+        p = h.next
+        while p:
+            if p.random:
+                p.next.random = p.random.next
+            p = p.next.next
+        p = h.next.next
+        while p:
+            if p.next:
+                p.next = p.next.next
+            p = p.next
+        return h.next.next
         
  
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(["eat", "tea", "tan", "ate", "nat", "bat"])
+something = S.function( buildList([0,9,1,2,4]),  buildList([3,2,4]))
 print(something)
--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-03-25 20:31:55
 LastEditors: Au3C2
-LastEditTime: 2021-04-26 10:14:00
+LastEditTime: 2021-07-22 13:34:36
 '''
 import collections    
 import heapq
@@ -21,7 +21,12 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+        
 def buildTree(data:list):
     if not data: return
     n = len(data)
