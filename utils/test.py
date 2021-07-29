3006077e7021a90e2dd9a8974cf67d99d55b9604
Au3C2 <au3c2@hotmail.com>
2021-07-29 12:46:11 +0800

    0729更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-07-28 10:24:51
+LastEditTime: 2021-07-29 10:35:23
 '''
 import copy
 import heapq
@@ -25,37 +25,26 @@ null = None
 
 class Solution:
     def __init__(self):
-        self.MAX_SIZE = 10 ** 9 + 7
+        self.POWER2 = [1]
+        for i in range(20):
+            self.POWER2.append(self.POWER2[-1]*2)
         pass
                     
-    def function(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        g = defaultdict(list)
-        def dfs(root):
-            if not root: return
-            if root.left:
-                g[root.val].append(root.left.val)
-                g[root.left.val].append(root.val)
-            if root.right:
-                g[root.val].append(root.right.val)
-                g[root.right.val].append(root.val)
-            dfs(root.left)
-            dfs(root.right)
-        dfs(root)
-        visited = [False] * 501
-        queue = deque([target.val])
-        for i in range(k):
-            n = len(queue)
-            for _ in range(n):
-                cur = queue.popleft()
-                visited[cur] = True
-                for val in g[cur]:
-                    if not visited[val]:
-                        queue.append(val)
-        return list(queue)
+    def function(self, label: int) -> List[int]:
+        for n in range(20):
+            if label < self.POWER2[n]:
+                break
+        # print(n)
+        n -= 1
+        ans = [label]
+        for i in range(n,0,-1):
+            ans.append(self.POWER2[i]-1-ans[-1]//2+self.POWER2[i-1])
+        
+        return ans[::-1]
  
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(buildTree([3,5,1,6,2,0,8,null,null,7,4]), TreeNode(5),2)
+something = S.function(label = 656356)
 print(something)
