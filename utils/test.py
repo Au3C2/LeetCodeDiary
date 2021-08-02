84cc5aa8f0d74d57a45b6adb6c44f941953100f9
Au3C2 <au3c2@hotmail.com>
2021-08-02 15:05:24 +0800

    0802更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-07-29 10:35:23
+LastEditTime: 2021-08-02 13:08:18
 '''
 import copy
 import heapq
@@ -25,26 +25,32 @@ null = None
 
 class Solution:
     def __init__(self):
-        self.POWER2 = [1]
-        for i in range(20):
-            self.POWER2.append(self.POWER2[-1]*2)
         pass
                     
-    def function(self, label: int) -> List[int]:
-        for n in range(20):
-            if label < self.POWER2[n]:
-                break
-        # print(n)
-        n -= 1
-        ans = [label]
-        for i in range(n,0,-1):
-            ans.append(self.POWER2[i]-1-ans[-1]//2+self.POWER2[i-1])
-        
-        return ans[::-1]
+    def function(self, times: List[List[int]], n: int, k: int) -> int:
+        g = defaultdict(list)
+        for t in times:
+            g[t[0]].append((t[1],t[2]))
+        q = deque([(k, 0)])
+        visited = {k:0}
+        while q:
+            q_len = len(q)
+            for _ in range(q_len):
+                cur, t = q.popleft()
+                for nei, w in g[cur]:
+                    if (nei in visited and w+t < visited[nei]) or nei not in visited:
+                        q.append((nei, w+t))
+                        visited[nei] = w+t
+        if len(visited) != n:
+            return -1
+        time = -1
+        for k, v in visited.items():
+            time = max(time,v)
+        return time
  
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(label = 656356)
+something = S.function(times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2)
 print(something)
