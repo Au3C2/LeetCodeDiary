0af5caf8d36eed1497abd32635862d4c8b7cae13
Au3C2 <au3c2@hotmail.com>
2021-08-05 16:05:11 +0800

    0805更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-02 13:08:18
+LastEditTime: 2021-08-05 15:59:18
 '''
 import copy
 import heapq
@@ -27,30 +27,27 @@ class Solution:
     def __init__(self):
         pass
                     
-    def function(self, times: List[List[int]], n: int, k: int) -> int:
-        g = defaultdict(list)
-        for t in times:
-            g[t[0]].append((t[1],t[2]))
-        q = deque([(k, 0)])
-        visited = {k:0}
+    def function(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        iG = [ [] for _ in range(n) ] # 反向图
+        ic = [0] * n # 图的入度图
+        for s, es in enumerate(graph): # start, end
+            for e in es:
+                iG[e].append(s)
+                # ic[e] += 1
+            ic[s] = len(es)
+        q = deque([i for i, n in enumerate(ic) if n == 0])
         while q:
-            q_len = len(q)
-            for _ in range(q_len):
-                cur, t = q.popleft()
-                for nei, w in g[cur]:
-                    if (nei in visited and w+t < visited[nei]) or nei not in visited:
-                        q.append((nei, w+t))
-                        visited[nei] = w+t
-        if len(visited) != n:
-            return -1
-        time = -1
-        for k, v in visited.items():
-            time = max(time,v)
-        return time
- 
+            cur = q.popleft()
+            for nei in iG[cur]:
+                ic[nei] -= 1
+                if ic[nei] == 0:
+                    q.append(nei)
+        return [i for i, n in enumerate(ic) if n == 0]
+
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2)
+something = S.function(graph = [[4,9],[3,5,7],[0,3,4,5,6,8],[7,8,9],[5,6,7,8],[6,7,8,9],[7,9],[8,9],[9],[]])
 print(something)
