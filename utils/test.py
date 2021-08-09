93fe7de86f82a4caed2b841e616c2eba55f362e5
Au3C2 <au3c2@hotmail.com>
2021-08-09 14:49:43 +0800

    0809更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-07 11:36:42
+LastEditTime: 2021-08-09 10:58:02
 '''
 import copy
 import heapq
@@ -27,26 +27,26 @@ class Solution:
     def __init__(self):
         pass
                     
-    def function(self, nums: List[int]) -> bool:
-        n = len(nums)
-        for i in range(n):
-            path = [-1] * n
-            path[i] = 0
-            next_i = (i+nums[i])%n
-            step = 0
-            while nums[i] * nums[next_i] > 0 and i != next_i: # 保证下一步是同号
-                if path[next_i] != -1 and step - path[next_i] > 0:
-                    return True
-                step += 1
-                path[next_i] = step
-                i, next_i = next_i, (next_i+nums[next_i])%n
-
-        return False
+    def function(self, n: int, primes: List[int]) -> int:
+        if n < 0:
+            return 0
+        dp = [float('inf')] * n
+        dp[0] = 1
+        np = len(primes)
+        index = [0] * np
+        for i in range(1, n):
+            for j in range(np):
+                if primes[j] * dp[index[j]] <= dp[i]:
+                    dp[i] = primes[j] * dp[index[j]]
+            for j in range(np):
+                if dp[i] == primes[j] * dp[index[j]]:
+                    index[j] += 1
+        return dp[n-1]
 
 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(nums = [3,1,2])
+something = S.function(n = 12, primes = [2,7,13,19])
 print(something)
