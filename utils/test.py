a7b0c92ade1b9b2f99cda696ebae887e9a0975aa
Au3C2 <au3c2@hotmail.com>
2021-08-12 10:54:42 +0800

    0812更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-10 11:02:27
+LastEditTime: 2021-08-12 10:41:52
 '''
 import copy
 import heapq
@@ -27,23 +27,24 @@ class Solution:
     def __init__(self):
         pass
                     
-    def function(self, nums: List[int]) -> int:
-        if (n:=len(nums)) < 3:
-            return 0
-        diff = [nums[i]-2*nums[i-1]+nums[i-2] for i in range(2,n)]
-        count, ans = 0, 0
-        for d in diff:
-            if d:
-                count = 0
-                continue
-            count += 1
-            ans += count            
-        return ans
+    def function(self, s: str) -> int:
+        n = len(s)
+        curRow, nxtRow = [0] * n, [0] * n
+        # 从下往上遍历
+        for i in range(n - 1, -1, -1):
+            curRow[i] = 1
+            for j in range(i + 1, n, 1):
+                if s[i] == s[j]:
+                    curRow[j] = nxtRow[j-1] + 2
+                else:
+                    curRow[j] = max(nxtRow[j],curRow[j-1])
+            nxtRow, curRow = curRow, [0] * n
+        return nxtRow[n - 1]
 
 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function([1,2,3,4,5,6,7,8,0,1,2,4,6,8,10])
+something = S.function(s = "cbbd")
 print(something)
