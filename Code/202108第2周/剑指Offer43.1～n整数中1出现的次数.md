6befcf66a5d5cd444e6fd806398e2adad53d1370
Au3C2 <au3c2@hotmail.com>
2021-08-13 13:26:10 +0800

    0813更新

---
+++ Code/202108第2周/剑指Offer43.1～n整数中1出现的次数.md
@@ -0,0 +1,59 @@
+<!--
+ * @Description: 
+ * @Autor: Au3C2
+ * @Date: 2021-08-13 13:22:43
+ * @LastEditors: Au3C2
+ * @LastEditTime: 2021-08-13 13:22:55
+-->
+# [剑指 Offer 43. 1～n 整数中 1 出现的次数](https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/)
+
+输入一个整数 `n` ，求1～n这n个整数的十进制表示中1出现的次数。
+
+例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+
+ 
+
+**示例 1：**
+
+```
+输入：n = 12
+输出：5
+```
+
+**示例 2：**
+
+```
+输入：n = 13
+输出：6
+```
+
+ 
+
+**限制：**
+
+-   `1 <= n < 2^31`
+
+注意：本题与主站 233 题相同：https://leetcode-cn.com/problems/number-of-digit-one/
+
+数学 困难
+
+[题解](https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/mian-shi-ti-43-1n-zheng-shu-zhong-1-chu-xian-de-2/)
+
+# 代码
+
+```python
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        digit, res = 1, 0
+        high, cur, low = n // 10, n % 10, 0
+        while high != 0 or cur != 0:
+            if cur == 0: res += high * digit
+            elif cur == 1: res += high * digit + low + 1
+            else: res += (high + 1) * digit
+            low += cur * digit
+            cur = high % 10
+            high //= 10
+            digit *= 10
+        return res
+```
+
--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-12 10:41:52
+LastEditTime: 2021-08-13 13:18:47
 '''
 import copy
 import heapq
@@ -26,25 +26,23 @@ null = None
 class Solution:
     def __init__(self):
         pass
-                    
-    def function(self, s: str) -> int:
-        n = len(s)
-        curRow, nxtRow = [0] * n, [0] * n
-        # 从下往上遍历
-        for i in range(n - 1, -1, -1):
-            curRow[i] = 1
-            for j in range(i + 1, n, 1):
-                if s[i] == s[j]:
-                    curRow[j] = nxtRow[j-1] + 2
-                else:
-                    curRow[j] = max(nxtRow[j],curRow[j-1])
-            nxtRow, curRow = curRow, [0] * n
-        return nxtRow[n - 1]
+    def function(self, n: int) -> int:
+        digit, res = 1, 0
+        high, cur, low = n // 10, n % 10, 0
+        while high != 0 or cur != 0: # 当 high 和 cur 同时为 0 时，说明已经越过最高位，因此跳出
+            if cur == 0: res += high * digit
+            elif cur == 1: res += high * digit + low + 1
+            else: res += (high + 1) * digit
+            low += cur * digit # 将 cur 加入 low ，组成下轮 low
+            cur = high % 10  # 下轮 cur 是本轮 high 的最低位
+            high //= 10  # 将本轮 high 最低位删除，得到下轮 high
+            digit *= 10 # 位因子每轮 × 10
+        return res
 
 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(s = "cbbd")
+something = S.function(824883294)
 print(something)
