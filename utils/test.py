b9564d10822068185e06e46e19326ddb033e7c7f
Au3C2 <au3c2@hotmail.com>
2021-08-16 18:30:49 +0800

    老题新做

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-14 11:34:14
+LastEditTime: 2021-08-16 18:15:49
 '''
 import copy
 import heapq
@@ -26,39 +26,21 @@ null = None
 class Solution:
     def __init__(self):
         pass
-    def function(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
-        n = len(preferences)
-        prefer_index = [[-1] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n-1):
-                prefer_index[i][preferences[i][j]] = j # 快速索引朋友的排序
-        pairs_d = [0] * n
-        for x, y in pairs:
-            pairs_d[x] = y
-            pairs_d[y] = x
-        unhappy = set()
-        
-        # 挨个检查每个人
-        for x in range(n):
-            if x in unhappy:
-                continue
-            y = pairs_d[x] # x 的对象
-            idx_y = prefer_index[x][y] # y在x的排序
-            # 搜索 x 中排在 y 前面的朋友，并且挨个检查
-            for u in preferences[x][:idx_y]:
-                # 检查u的配对
-                v = pairs_d[u]
-                if prefer_index[u][x] < prefer_index[u][v]:
-                    unhappy.add(x)
-                    unhappy.add(u)
-        return len(unhappy)
-
+    def function(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9 + 7
+        ng = len(group)
+        dp = [[0] * (minProfit+1) for _ in range(n+1)]
+        for j in range(n+1):
+            dp[j][0] = 1
+        for p, g  in zip(profit, group):
+            for j in range(n, g-1, -1):
+                for k in range(minProfit, -1, -1):
+                    dp[j][k] = (dp[j-g][max(k-p,0)] + dp[j][k]) % MOD
+        return dp[n][minProfit]
 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(4,
-[[1,3,2],[0,2,3],[3,1,0],[2,0,1]],
-[[2,1],[3,0]])
+something = S.function(n = 5, minProfit = 3, group = [2,2], profit = [2,3])
 print(something)
