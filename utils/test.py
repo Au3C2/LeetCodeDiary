ac58bf1f426277465ace391dc8fa01cf8b20942c
Au3C2 <au3c2@hotmail.com>
2021-08-22 14:22:06 +0800

    202108第3周更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-16 18:15:49
+LastEditTime: 2021-08-20 14:23:38
 '''
 import copy
 import heapq
@@ -16,7 +16,7 @@ from typing import *
 
 import numpy as np
 from scipy.special import comb, perm
-from sortedcontainers import SortedList
+# from sortedcontainers import SortedList
 
 from utils import *
 
@@ -26,21 +26,25 @@ null = None
 class Solution:
     def __init__(self):
         pass
-    def function(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        MOD = 10**9 + 7
-        ng = len(group)
-        dp = [[0] * (minProfit+1) for _ in range(n+1)]
-        for j in range(n+1):
-            dp[j][0] = 1
-        for p, g  in zip(profit, group):
-            for j in range(n, g-1, -1):
-                for k in range(minProfit, -1, -1):
-                    dp[j][k] = (dp[j-g][max(k-p,0)] + dp[j][k]) % MOD
-        return dp[n][minProfit]
+    def function(self, s: str, k: int) -> str:
+        s = list(s)
+        n = len(s)
+        i = -1
+        for i in range(n//(2*k)):
+            t = s[i*2*k:i*2*k+k]
+            s[i*2*k:i*2*k+k] = t[::-1]
+        i += 1
+        if n % (2*k) >= k:
+            t = s[i*2*k:i*2*k+k]
+            s[i*2*k:i*2*k+k] = t[::-1]
+        else:
+            t = s[i*2*k:]
+            s[i*2*k:] = t[::-1]
+        return ''.join(s)
 
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(n = 5, minProfit = 3, group = [2,2], profit = [2,3])
+something = S.function("abcd",3)
 print(something)
