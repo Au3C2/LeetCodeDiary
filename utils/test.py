5e2d00403dcf7a7351933c60996419adf27c078b
Au3C2 <au3c2@hotmail.com>
2021-08-27 13:00:18 +0800

    0827更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-26 11:17:11
+LastEditTime: 2021-08-26 15:32:31
 '''
 import copy
 import heapq
@@ -26,22 +26,27 @@ null = None
 class Solution:
     def __init__(self):
         pass
-    def function(self, people: List[int], limit: int) -> int:
-        people.sort(reverse=True)
-        n = len(people)
-        ans, lp, rp = 0, 0, n-1
-        while lp <= rp:
-            if lp == rp:
-                rp -= 1
-            if people[rp] + people[lp] <= limit:
-                rp -= 1
-            ans += 1
-            lp += 1
-        return ans         
+    def function(self, n:int, m:int, nums):
+        ans = [float('inf')] * (n*m) # 返回的答案
+        t = [(nums[j][0],j,0) for j in range(n)]  # 多指针
+        heapq.heapify(t)
+        for i in range(n * m):
+            ans[i], j, idx = heapq.heappop(t)
+            if idx < m - 1:
+                idx += 1
+                heapq.heappush(t,(nums[j][idx], j, idx))
+            # idx = 0
+            # for j in range(n): # 遍历查找 nums 每行
+            #     if t[j] < m and nums[j][t[j]] <= ans[i]:
+            #         idx = j
+            #         ans[i] = nums[j][t[j]]
+            # t[idx] += 1 # 当前指针向后移
+
+        return ans
             
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
 S = Solution()
-something = S.function(people = [3,5,3,4], limit = 5)
+something = S.function(3,3,[[1,3,5],[2,4,6],[1,3,6]])
 print(something)
