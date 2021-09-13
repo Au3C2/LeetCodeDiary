471e856e5a8e8e3c5e3d3b44d4853b62e20f1ab7
Au3C2 <au3c2@hotmail.com>
2021-09-13 16:06:03 +0800

    202109第2周更新

--- utils/test.py
+++ utils/test.py
@@ -3,7 +3,7 @@ Description:
 Autor: Au3C2
 Date: 2021-06-15 10:05:35
 LastEditors: Au3C2
-LastEditTime: 2021-08-29 23:43:41
+LastEditTime: 2021-09-13 15:45:41
 '''
 import copy
 import heapq
@@ -26,15 +26,17 @@ null = None
 class Solution:
     def __init__(self):
         pass
-    def function(self, arr: List[int]) -> int:
-        n = len(arr)
-        preSum = [0] * (n+1)
-        for i in range(n):
-            preSum[i+1] = preSum[i] + arr[i]
+    def function(self, points: List[List[int]]) -> int:
         ans = 0
-        for w in range(1,n+1,2):
-            for i in range(n-w+1):
-                ans += (preSum[i+w] - preSum[i])
+        for [xi, yi] in points:
+            d = Counter()
+            for [xj, yj] in points:
+                if xi == xj and yi == yj: continue
+                dist = (xi-xj)**2 + (yi-yj)**2
+                d[dist] += 1
+            for _, v in d.items():
+                if v >= 2:
+                    ans += v * (v-1)
         return ans
 
         
@@ -42,6 +44,7 @@ class Solution:
 # root1 = buildTree([1])
 # root2 = buildTree([2])
 # head = buildList([1,2,3,4,5])
+
 S = Solution()
-something = S.function([1,4,2,5,3])
+something = S.function([[0,0],[1,0],[-1,0],[0,1],[0,-1]])
 print(something)
